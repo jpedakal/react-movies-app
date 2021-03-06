f05a953b97399a233089f7eff4b0342cb5e99c65
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-06 16:16:11 +0530

    update

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 import products from '../components/products';
 
 const HomeScreen = () => {
@@ -8,9 +9,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             <Row>
-                {products.map(item => (
-                    <Col>
-                    {item.name}
+                {products.map((item) => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Product product={item} />
                     </Col>
                 ))}
             </Row>
