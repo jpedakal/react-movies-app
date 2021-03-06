e682e4639d7ba68baf31ed4375954062d7026abb
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-06 11:02:58 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home'
+import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
