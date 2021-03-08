8e4ebd81d51d505bff3190b966e1b7804fe0472c
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-08 17:15:06 +0530

    Update ProductScreen.js

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -9,12 +9,32 @@ const ProductScreen = ({ match }) => {
     console.log(product)
     return (
         <>
-            <Link className='btn btn-danger my-3' to='/'>Go Back</Link>
             <Row>
-                <Col md={6}>
-                    
+                <Col md={4}>
+                    <Image width='360' height='470' src={product.image} alt={product.name} />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h4>{product.name}</h4>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={product.numReviews} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h5>${product.price}</h5>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Brand : {product.brand}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Category : {product.category}
+                        </ListGroup.Item>
+                        <ListGroup.Item>{product.description}</ListGroup.Item>
+                    </ListGroup>
                 </Col>
             </Row>
+            <Link className='btn btn-danger my-3' to='/'>Go Back</Link>
         </>
     )
 }
