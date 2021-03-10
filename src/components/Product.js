cef120e9dd63ebe7c2286d902519f648f6311986
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-10 21:02:07 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,6 @@ import Rating from './Rating';
 
 
 const Product = ({ product }) => {
-    
     return (
         <Card className='my-3 p-3 ronded'>
             <Link to={`/product/${product._id}`}>
