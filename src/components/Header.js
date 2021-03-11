5b97f2d9c5a7ccc6aea5567a4e7600e91fd6fb2d
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-11 20:40:52 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
+    
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
