abf42e48ae6101ddefc9263d7aca713c33b3423f
jpedakal <40006634+jpedakal@users.noreply.github.com>
2021-03-20 16:27:04 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,3 +2,8 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const store = createStore(reducer, initialState)
